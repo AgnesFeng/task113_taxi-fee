351a3e74598ec982b10faf0bc44603373b977408
fengyu_u <395861552@qq.com>
2017-12-02 22:07:21 +0800

    finished

--- main/main.js
+++ main/main.js
@@ -1,4 +1,23 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(input,time) {
+    let price;
+    let priceTime;
+    let str;
+    if(time ===0){
+        priceTime = 0;
+    }else{
+        priceTime = time * 0.25;
+        console.log(priceTime);
+    }
+    if(input <=2){
+        price = 6;
+        //str = '总计：' + price.toFixed(0) + '元';
+
+    }else if(input <=8){
+        price = 6 + Math.round(input-2) * 0.8;
+    }else{
+        price = 6 + 6*0.8 + Math.round(input-8)*0.8*1.5;
+    }
+    console.log(price);
+    str = '总计：' + Math.round(price+priceTime) + '元';
+    return str;
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -1,5 +1,25 @@
+let expect = require("chai").expect;
+
 const main = require('../main/main');
 
 describe('taxi fee', function () {
-    // write your tests here...
+    it('<=2km',function () {
+        let input = 2;
+        let time = 4;
+        let result = main(input,time);
+        expect(result).to.equal("总计：7元");
+    });
+    it('>2km,<=8km',function () {
+        let input = 3.5;
+        let time = 4;
+        let result = main(input,time);
+        expect(result).to.equal("总计：9元");
+    });
+    it('>8km',function () {
+        let input = 34.44;
+        let time = 4;
+        let result = main(input,time);
+
+        expect(result).to.equal("总计：43元");
+    });
 });
